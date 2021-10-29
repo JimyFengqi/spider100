3598cd98963583f411a798ab9bfb5d90d2b9ecd4
JimyFengqi <jmps515@163.com>
2021-10-29 22:47:05 +0800

    添加一个文件

--- biying/down_bing.py
+++ biying/down_bing.py
@@ -16,12 +16,15 @@ def create_dir(dir_path):
         print('current path not exist[%s]' % dir_path)
         os.makedirs(dir_path)
 
+gallery_name = 'gallery'
+bing_name = 'bing'
 
 HOME = os.getcwd()
-HOME = os.path.join(HOME, 'gallery')
-pic_dir = os.path.join(HOME, 'bing')
+HOME = os.path.join(HOME, gallery_name)
+pic_dir = os.path.join(HOME, bing_name)
 create_dir(pic_dir)
 bing_json_file = os.path.join(pic_dir, 'bing.json')
+
 
 date = datetime.datetime.now().strftime('%Y%m%d')
 yesterday = pic_dir + str(int(date) - 1)
@@ -33,6 +36,7 @@ urllib.request.urlretrieve(json_url, bing_json_file)
 
 with open(bing_json_file, "r", encoding='utf-8') as f:
     bing_json = json.load(f)
+
 
 imges = bing_json['images']
 for image in imges:
